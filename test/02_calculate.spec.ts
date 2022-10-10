d6a33e6bc670540e7a5d3848d8a2f94f54156a6d
Neil Shevlin <neilshevlin.98@gmail.com>
2022-10-10 15:28:35 +0100

    Added tests for calculator

--- test/02_calculate.spec.ts
+++ test/02_calculate.spec.ts
@@ -8,8 +8,8 @@ chai.use(chaiHttp)
 
 const expect = chai.expect
 
-describe('calculatorRoute', async () => {
-    it('should respond with a status 404 because there is an invalid string', () => {
+describe('calculatorRoute',  () => {
+    it('should respond with a status 404 because there is an invalid string', async () => {
         return chai
             .request(app)
             .get('/calculator')
@@ -17,12 +17,66 @@ describe('calculatorRoute', async () => {
                 expect(res.status).to.be.equal(404)
         })
     })
-    it('should respond with a status 200 because there is a valid string', () => {
+    it('should respond with a status 200 because there is a valid string', async () => {
         return chai
             .request(app)
             .get('/calculator')
             .then(res => {
+                expect(res.status).to.be.equal(404)
+        })
+    })
+    it('should respond with a status 200 because there is a valid string', async () => {
+        return chai
+            .request(app)
+            .get('/calculator')
+            .send({ expression: '1+1' })
+            .then(res => {
                 expect(res.status).to.be.equal(200)
+        })
+    })
+    it('Should assert that the response is 2 given the input 1+1', async () => {
+        return chai
+            .request(app)
+            .get('/calculator')
+            .send({ expression: '1+1' })
+            .then(res => {
+                expect(res.body.result).to.be.equal(2)
+        })
+    })
+    it('Should assert that the response is 2 given the input 4-2', async () => {
+        return chai
+            .request(app)
+            .get('/calculator')
+            .send({ expression: '4-2' })
+            .then(res => {
+                expect(res.body.result).to.be.equal(2)
+        })
+    })
+    it('Should assert that the response is 4 given the input 2*2', async () => {
+        return chai
+            .request(app)
+            .get('/calculator')
+            .send({ expression: '2*2' })
+            .then(res => {
+                expect(res.body.result).to.be.equal(4)
+        })
+    })
+    it('Should assert that the response is 20 given the input 10+10', async () => {
+        return chai
+            .request(app)
+            .get('/calculator')
+            .send({ expression: '10+10' })
+            .then(res => {
+                expect(res.body.result).to.be.equal(20)
+        })
+    })
+    it('Should assert that the response is 2 given the input 4/2', async () => {
+        return chai
+            .request(app)
+            .get('/calculator')
+            .send({ expression: '4/2' })
+            .then(res => {
+                expect(res.body.result).to.be.equal(2)
         })
     })
 })
